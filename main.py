488c6f451a1ec08137019166057f0aa90c5bf327
pronoob199 <166720091+pronoob199@users.noreply.github.com>
2024-04-18 14:25:15 +0200

    feat: added search user

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-from utils.crud import read, create_user
-from models.data import users
+     from utils.crud import read, create_user, seeach
+    from models.data import users
 
 
-if __name__ == '_main_':
+    if __name__ == '_main_':
 
-    print(f"Witaj {users[0]['name']}!")
-    while True:
-        print("Menu:")
-        print("0. Zakończ program:")
-        print("1. Pokaż co u znajomych: ")
-        print("2. Dodaj znajomego: ")
-        menu_option:str=input("Wybierz dostępną funkcje z menu: ")
-        if menu_option=="0":
-            break
-        if menu_option == "1":
-            read(users)
-        if menu_option == "2":
-            create_user(users)
-
+        print(f"Witaj {users[0]['name']}!")
+        while True:
+            print("Menu:")
+            print("0. Zakończ program:")
+            print("1. Pokaż co u znajomych: ")
+            print("2. Dodaj znajomego: ")
+            print("3. Wyszukaj znajomego: ")
+            menu_option:str=input("Wybierz dostępną funkcje z menu: ")
+            if menu_option=="0":
+                break
+            if menu_option == "1":
+                read(users)
+            if menu_option == "2":
+                create_user(users)
+            if menu_option == "3":
+                seeach(users)
 
 
 
