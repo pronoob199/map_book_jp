45ee14c0ecf5a12691dde9f796ee0c201350fbff
pronoob199 <166720091+pronoob199@users.noreply.github.com>
2024-04-18 13:58:46 +0200

    feat: added create users

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utils.crud import read
+from utils.crud import read, create_user
 from models.data import users
+
 
 if __name__ == '_main_':
 
@@ -8,11 +9,16 @@ if __name__ == '_main_':
         print("Menu:")
         print("0. Zakończ program:")
         print("1. Pokaż co u znajomych: ")
+        print("2. Dodaj znajomego: ")
         menu_option:str=input("Wybierz dostępną funkcje z menu: ")
         if menu_option=="0":
             break
         if menu_option == "1":
             read(users)
+        if menu_option == "2":
+            create_user(users)
+
+
 
 
 
--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,20 @@
-def read(users: list[dict]) -> None:
+def read(users:list[dict])->None:
     for user in users[1:]:
         print(f'Twój znajomy {user["name"]} opublikował: {user["posts"]}')
+   users: list[dict] = [
+        {"name": "Jakub", "surname": "Praski", "posts": 20},
+        {"name": "Janek", "surname": "Mielec", "posts": 1},
+        {"name": "Maciej", "surname": "Przybytek", "posts": 45},
+        {"name": "Bartosz", "surname": "Pietrasik", "posts": 30},
+        {"name": "Tymoteusz", "surname": "Miszczak", "posts": 15},
+        {"name": "Mateusz", "surname": "Matysiak", "posts": 10},
+
+    name:str=input("Podaj imie uzytkownika: ")
+    surnmame: str=input("Podaj nazwisko użytkownika: ")
+    posts: str=input("Podaj lczbę postów:")
+    user:dict={ "name": name, "surname": surname, "posts": posts}
+    users.append(user)
+
+create_users(users)
+
+print(users)
